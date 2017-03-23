afab30c602b9ac8f27f33874af781cf75a1807ae
Luca Srdjenovic <luca.srdjenovic@he-arc.ch>
2017-03-23 12:30:17 +0100

    Ajout livre

---
+++ source/pycrypto/Example/aes_des.py
@@ -0,0 +1,25 @@
+from Crypto.Cipher import AES
+from Crypto.Cipher import DES
+from Crypto import Random
+
+iv_AES = Random.new().read(AES.block_size) #Vetor d'initialisation
+iv_DES = Random.get_random_bytes(8) #Vetor d'initialisation
+
+key_AES = 'abcdefghijklmnop'
+key_DES = 'abcdefgh'
+
+aese = AES.new(key_AES,AES.MODE_CFB,iv_AES)
+aesd = AES.new(key_AES,AES.MODE_CFB,iv_AES)
+dese = DES.new(key_DES,DES.MODE_CFB,iv_DES)
+desd = DES.new(key_DES,DES.MODE_CFB,iv_DES)
+
+plaintext = 'Hello! World'
+
+plaintext = aesd.decrypt(aese.encrypt(plaintext))
+
+print (plaintext)
+
+plaintext = aesd.decrypt(dese.encrypt(plaintext))
+
+print (plaintext)
+
